b29e4b3cf945f4cd7626bdd9dc73dff6cad090a5
Larry <lhe@successfactors.com>
2013-04-06 20:26:24 -0500

    reorganize the structure of folder and add default config file for daemon process

--- extras/htpasswd.c
+++ extras/htpasswd.c
@@ -49,7 +49,7 @@ static void getword(char *word, char *line, char stop) {
     while((line[y++] = line[x++]));
 }
 
-static int getline(char *s, int n, FILE *f) {
+static int getline_ext(char *s, int n, FILE *f) {
     register int i=0;
 
     while(1) {
@@ -189,7 +189,7 @@ int main(int argc, char *argv[]) {
     strcpy(user,argv[2]);
 
     found = 0;
-    while(!(getline(line,MAX_STRING_LEN,f))) {
+    while(!(getline_ext(line,MAX_STRING_LEN,f))) {
         if(found || (line[0] == '#') || (!line[0])) {
             putline(tfp,line);
             continue;
